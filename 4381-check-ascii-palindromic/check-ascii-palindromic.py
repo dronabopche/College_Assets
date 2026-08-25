b300a9034860a012296c8198d4ca78e9d2266428
Drona Bopche <161079263+dronabopche@users.noreply.github.com>
2026-08-25 12:09:33 +0530

    Time: 4 ms (24.83%) | Memory: 19.4 MB (40.45%) - LeetSync

--- 4381-check-ascii-palindromic/check-ascii-palindromic.py
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.py
@@ -4,15 +4,10 @@ class Solution:
         binary_str = ""
         
         for char in s:
-            print(char)
             ascii_char = ord(char)
-            print(ascii_char)
-
             ascii_bin = format(ascii_char, "08b")
-            print(ascii_bin)
 
             binary_str += str(ascii_bin)
-            print(binary_str)
 
         if binary_str == binary_str[::-1]:
             return True
